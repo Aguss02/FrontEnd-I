41ed34b1ac501ffff5baf0dde34a0343c0325ffa
Aguss02 <tosi.agustina02@gmail.com>
2022-08-14 18:11:52 -0300

    Primer Commit

---
+++ clase5/script.js
@@ -0,0 +1,10 @@
+
+// const temaOscuro = confirm("¿Desea aplicar el tema oscuro?")
+
+// if (temaOscuro) {
+//     document.querySelector('body').classList.add('dark');
+// }
+
+function cambiarTema(){
+    document.querySelector('body').classList.toggle('dark')
+}
